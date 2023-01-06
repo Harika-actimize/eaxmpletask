c02617b08a898de4030cbf10ee7fcf90204c8a4b
harika <harika.actimize@gmail.com>
2023-01-06 18:19:11 +0530

    “new”

--- src/App.js
+++ src/App.js
@@ -9,15 +9,6 @@ class App extends Component {
     return (
     <Router>
         <div>
-          {/* <h2>Welcome to React Router Tutorial</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-            <li><Link to={'/about'} className="nav-link">About</Link></li>
-          </ul>
-          </nav>
-          <hr /> */}
           <Switch>
               <Route exact path='/' component={ProductSlider} />
               <Route exact path='/productdetails/:imagename/:id' component={ProductDetails} />
